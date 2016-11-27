51dd68799d235adbd08322e71da4231dfe7e04ed
simplon-mars-2-nadege-s <princessgirlyfactory@gmail.com>
2016-11-27 13:31:42 +0100

    Update cercle-perimetre-aire.js

--- poo/cercle-perimetre-aire.js
+++ poo/cercle-perimetre-aire.js
@@ -21,3 +21,28 @@ console.log("Son aire vaut " + cercle.aire());
 La réponse : 
 Son périmètre vaut 22.619467080000003
 Son aire vaut 40.71504074400001
+
+
+/*
+Autre solution  : Exercice : objet cercle
+*/
+
+var r = Number(prompt("Entrez le rayon d'un cercle :"));
+
+var cercle = {
+    rayon: r,
+
+    // Renvoie le périmètre du cercle
+    perimetre: function () {
+        return 2 * this.rayon * Math.PI;
+    },
+    // Renvoie l'aire du cercle
+    aire: function () {
+        return this.rayon * this.rayon * Math.PI;
+        // Autre possibilité
+        //return Math.pow(this.rayon, 2) * Math.PI;
+    }
+};
+
+console.log("Son périmètre vaut " + cercle.perimetre());
+console.log("Son aire vaut " + cercle.aire());
