4066a5a5f154ffe6038e3286b4a20a22b0c49279
cesi-RIL-2016-nadege <princessgirlyfactory@gmail.com>
2016-12-10 09:12:59 +0100

    Create introduction.js

---
+++ function/introduction.js
@@ -0,0 +1,12 @@
+// Créer et appeler une fonction
+
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+ 
+ourReusableFunction();
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
